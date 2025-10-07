5d709884b3060bd52bbd41a07a66abbf44f0bd69
Vrushali Kottkona <vrushalimk9@gmail.com>
2025-10-07 19:20:10 +0530

    Testing

--- src/main/java/com/example/BankingApp/BankingAppApplication.java
+++ src/main/java/com/example/BankingApp/BankingAppApplication.java
@@ -8,6 +8,7 @@ public class BankingAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingAppApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
